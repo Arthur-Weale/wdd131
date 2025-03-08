1b7588c455cc2db9308a5f4950ef34bc2d713904
Arthur Weale <ajayrossy340@gmail.com>
2025-03-08 18:12:08 +0200

    Project complete and looking good

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+const currentYear = document.querySelector('#currentYear');
+const lastModified = document.querySelector('#lastModified');
+const current = new Date().getFullYear();
+const lastMod = document.lastModified;
+
+currentYear.textContent = current;
+lastModified.textContent = `last modified: ${lastMod.toLocaleString()}`;
